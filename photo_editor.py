9751093efda45627a09f47281a4cc4762bad2e9f
若tong <2434062669@qq.com>
2019-07-30 21:08:59 +0800

    Update photo_editor.py

--- photo_editor.py
+++ photo_editor.py
@@ -7,15 +7,19 @@ Created on Sat Jul 27 17:00:51 2019
 """
 
 import sys
-from PyQt5 import QtWidgets
-import main_window
+from PyQt5 import QtWidgets, QtGui
+from other_windows import CommonHelper, FramelessWindow
+from main_window import myWindow
+
 
 if __name__ == '__main__':
-    if not QtWidgets.QApplication.instance():
-        app = QtWidgets.QApplication(sys.argv)
-    else:
-        app = QtWidgets.QApplication.instance()
-    app.aboutToQuit.connect(app.deleteLater)
-    mywindow = main_window.myWindow()
-    mywindow.show()
-    app.exec_()
+    app = QtWidgets.QApplication(sys.argv)
+    qssStyle = CommonHelper.readQss('./ui_files/style.qss')
+    mainWnd = FramelessWindow()
+    mainWnd.setFrameSize(600,400)
+    mainWnd.setWindowTitle('Photo Editor')
+    mainWnd.setStyleSheet(qssStyle)
+    mainWnd.setWindowIcon(QtGui.QIcon('./ui_files/source/246px-Adobe_Photoshop_CC_icon.png'))
+    mainWnd.setWidget(myWindow(mainWnd))  # 把自己的窗口添加进来
+    mainWnd.show()
+    sys.exit(app.exec_())
